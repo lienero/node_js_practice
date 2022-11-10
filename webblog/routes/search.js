71c5164c330c09678520ee638c7f11470aada8bb
이경민 <lienero@naver.com>
2022-11-10 18:12:51 +0900

    페이징 기능 추가

--- webblog/routes/search.js
+++ webblog/routes/search.js
@@ -1,25 +1,37 @@
-var { CONNECTION_URL, OPTIONS, DATABASE } = require('../config/mongodb.config.js');
-var router = require('express').Router();
-var MongoClient = require('mongodb').MongoClient;
+let { CONNECTION_URL, OPTIONS, DATABASE } = require('../config/mongodb.config.js');
+let { MAX_ITEM_PER_PAGE } = require('../config/app.config.js').search;
+let router = require('express').Router();
+let MongoClient = require('mongodb').MongoClient;
 
 router.get('/*', (req, res) => {
+  let page = req.query.page ? parseInt(req.query.page) : 1;
   let keyword = req.query.keyword || '';
 
   // RegExp : 정규표현식, 객체는 리터럴 표기법과 생성자로써 생성할 수 있습니다.
   let regexp = new RegExp(`.*${keyword}.*`);
+  let query = { $or: [{ title: regexp }, { content: regexp }] };
 
   MongoClient.connect(CONNECTION_URL, OPTIONS, (error, client) => {
     let db = client.db(DATABASE);
-    db.collection('posts')
-      .find({
-        $or: [{ title: regexp }, { content: regexp }],
-      })
-      .sort({ published: -1 })
-      .toArray()
-      .then((list) => {
+    Promise.all([
+      db.collection('posts').find(query).count(),
+      db
+        .collection('posts')
+        .find(query)
+        .sort({ published: -1 })
+        .skip((page - 1) * MAX_ITEM_PER_PAGE)
+        .limit(MAX_ITEM_PER_PAGE)
+        .toArray(),
+    ])
+      .then((results) => {
         let data = {
           keyword,
-          list,
+          count: results[0],
+          list: results[1],
+          pagination: {
+            max: Math.ceil(results[0] / MAX_ITEM_PER_PAGE),
+            current: page,
+          },
         };
         res.render('./search/list.ejs', data);
       })
