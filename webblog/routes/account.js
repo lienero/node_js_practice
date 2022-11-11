db249fce7624c0912cd21c4a35043fa1eb35b13e
이경민 <lienero@naver.com>
2022-11-11 18:28:52 +0900

    회원등록 확인화면 구현

--- webblog/routes/account.js
+++ webblog/routes/account.js
@@ -1,5 +1,29 @@
 let router = require('express').Router();
 
+// 에러처리
+let validateRegistData = function (body) {
+  let isValidated = true,
+    errors = {};
+
+  if (!body.url) {
+    isValidated = false;
+    errors.url = "URLが未入力です。'/'から始まるURLを入力してください。";
+  }
+
+  if (body.url && /^\//.test(body.url) === false) {
+    isValidated = false;
+    errors.url = "'/'から始まるURLを入力してください。";
+  }
+
+  if (!body.title) {
+    isValidated = false;
+    errors.title = 'タイトルが未入力です。任意のタイトルを入力してください。';
+  }
+
+  // isValidated가 true면 undefined 반환, false 면 errors 반환
+  return isValidated ? undefined : errors;
+};
+
 let createRegistData = function (body) {
   let datetime = new Date();
   return {
@@ -24,6 +48,16 @@ router.get('/posts/regist', (req, res) => {
 router.post('/posts/regist/input', (req, res) => {
   let original = createRegistData(req.body);
   res.render('./account/posts/regist-form.ejs', { original });
+});
+
+router.post('/posts/regist/confirm', (req, res) => {
+  let original = createRegistData(req.body);
+  let errors = validateRegistData(req.body);
+  if (errors) {
+    res.render('./account/posts/regist-form.ejs', { errors, original });
+    return;
+  }
+  res.render('./account/posts/regist-confirm.ejs', { original });
 });
 
 module.exports = router;
